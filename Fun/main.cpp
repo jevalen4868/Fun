8f07a861a6aa05875f717d97d6e313dbec6de87d
Jeremy Valenzuela <jeremyvalenzuela@macos10113nouser-e11eeb55-06a2-52dd-a5d7-651f0bce18dc.local>
2016-04-13 08:46:35 -0500

    Initial Commit

---
+++ Fun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fun
+//
+//  Created by Jeremy Valenzuela on 4/13/16.
+//  Copyright © 2016 Jeremy Valenzuela. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
